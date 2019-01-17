05567ba4ecd3b6ee0fb8d6135b40bd728290e9e3
Guandao Yang <gy46@cornell.edu>
2019-01-17 05:03:13 -0500

    add data folder option

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 out_file=${1}
-python train.py --output_directory=${out_file} --log_directory=logdir
+data_dir=${2}
+python train.py --output_directory=${out_file} --log_directory=logdir --hparams=training_files=${data_dir}/train.txt,validation_files=${data_dir}/eval.txt
